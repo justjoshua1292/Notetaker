29373144923457ef6663d9e90f705b4e38b543a5
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-18 10:40:03 -0400

    adding api notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-
+const fs = require('fs');
 const path = require('path')
 
 // initialize an express application
 const app = express();
 
+// middleware
+app.use(express.static('public'));
+
+
+// routes
+
+// setting up API routes
+app.get('/api/notes', (req, res) => {
+   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        res.json(JSON.parse(data))
+   })
+})
+
+// setting up HTML routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
@@ -14,12 +28,9 @@ app.get('/notes', (req, res) => {
 })
 
 
-
-
 app.listen(3001, () => {
     console.log('App is now listening to PORT 3001!')
 })
-
 
 
 /*
